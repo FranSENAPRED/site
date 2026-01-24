43d99c74615ebd6e45d13eb7f2d2a32e1eb8f74d
FranSENAPRED <fpaez7@gmail.com>
2026-01-24 20:51:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const cards = [
   { icon:"🔔", value:"6", label:"Regiones con alertas", detail:"2 Roja, 1 Amarilla y 3 Temprana Preventiva" },
-  { icon:"🔥", value:"63", label:"Incendios activos", detail:"15 en combate, 46 controlados y 2 extinguidos" },
+  { icon:"🔥", value:"69", label:"Incendios activos", detail:"14 en combate, 34 controlados y 21 extinguidos" },
   { icon:"📢", value:"214", label:"Mensajes SAE enviados", detail:"Desde el 17/01/2026" },
-  { icon:"📋", value:"2.956", label:"FIBE", detail:"Hogares con ficha terminada" },
-  { icon:"👥", value:"20.485", label:"Personas damnificadas", detail:"689 personas albergadas" },
-  { icon:"🚁", value:"147", label:"Aeronaves desplegadas", detail:"CONAF, SENAPRED, EMCO y empresas forestales" },
+  { icon:"📋", value:"2.707", label:"FIBE", detail:"Hogares con ficha terminada" },
+  { icon:"👥", value:"20.568", label:"Personas damnificadas", detail:"682 personas albergadas" },
+  { icon:"🚁", value:"145", label:"Aeronaves desplegadas", detail:"CONAF, SENAPRED, EMCO y empresas forestales" },
   { icon:"👩‍🚒", value:"+4.900", label:"Bomberos movilizados", detail:"Recursos de respuesta y combate" },
   { icon:"🤝", value:"1.303", label:"Voluntarios desplegados", detail:"Apoyo humanitario y rehabilitación" }
 ];
