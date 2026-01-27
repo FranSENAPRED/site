856b857a9bac7e787b4c6056ad2624bdb326540a
FranSENAPRED <fpaez7@gmail.com>
2026-01-27 08:13:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const cards = [
   { icon:"🕊️", value:"21", label:"Personas fallecidas", detail:"Informado por PDI" },
-  { icon:"🏥", value:"330", label:"Personas lesionadas", detail:"Informado por SENAPRED-MINSAL" },
-  { icon:"👥", value:"21.712", label:"Personas damnificadas", detail:"394 personas albergadas" },
-  { icon:"🏚️", value:"3.074", label:"Viviendas destruidas", detail:"FIBE" },
+  { icon:"🏥", value:"333", label:"Personas lesionadas", detail:"Informado por SENAPRED-MINSAL" },
+  { icon:"👥", value:"21.713", label:"Personas damnificadas", detail:"450 personas albergadas" },
+  { icon:"🏚️", value:"3.271", label:"Viviendas destruidas", detail:"FIBE" },
   { icon:"📢", value:"214", label:"Mensajes SAE enviados", detail:"Desde el 17/01/2026" },
-  { icon:"🚁", value:"140", label:"Aeronaves desplegadas", detail:"CONAF, SENAPRED, EMCO y empresas forestales" },
+  { icon:"🚁", value:"137", label:"Aeronaves desplegadas", detail:"CONAF, SENAPRED, EMCO y empresas forestales" },
   { icon:"👩‍🚒", value:"+4.900", label:"Bomberos movilizados", detail:"Recursos de respuesta y combate" },
   { icon:"🤝", value:"1.303", label:"Voluntarios desplegados", detail:"Apoyo humanitario y rehabilitación" }
 ];
